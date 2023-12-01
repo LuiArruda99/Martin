932ce3071856b8a102dde7c4449fa64ece09f666
Luiz Claudio <luizclaudioarruda@outlook.com>
2023-12-01 17:19:14 -0300

    Update EmptyState.js

--- app/components/EmptyState.js
+++ app/components/EmptyState.js
@@ -1,6 +1,6 @@
 export default function EmptyState({ setOpen, setPrompt }) {
   return (
-    <div className="mt-12 sm:mt-24 space-y-6 text-gray-400 text-base mx-8 sm:mx-4 sm:text-2xl leading-12">
+    <div className="mt-12 sm:mt-24 space-y-10 text-gray-400 text-base mx-8 sm:mx-4 sm:text-2xl leading-12">
       <p>
         {" "}
         Customize a personalidade do Martin clicando em{" "}
